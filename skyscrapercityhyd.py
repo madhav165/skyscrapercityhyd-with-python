9a36ca1f0f29c946e9e2c0ed48e3dd8682d92a1d
Madhav Kandukuri <madhav165@gmail.com>
2016-06-27 19:15:07 +0530

    Now indents quotes

--- skyscrapercityhyd.py
+++ skyscrapercityhyd.py
@@ -4,6 +4,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 import os
+import contextlib
 
 global URL
 
@@ -23,8 +24,21 @@ def set_last_page(last_page):
         f.write(last_page)
 
 def get_html():
-    with urllib.request.urlopen(URL) as response:
-        return response.read();
+    #ua = UserAgent()
+    #ua.chrome
+    req = urllib.request.Request(URL,
+                                headers={'User-Agent': 'Mozilla/5.0 \
+                                         (X11; Linux x86_64) \
+                                          AppleWebKit/537.36 (KHTML, like Gecko) \
+                                          Chrome/53.0.2774.3 Safari/537.36'})
+    with contextlib.closing(urllib.request.urlopen(req)) as f:
+        return f.read().decode('ISO-8859-1')
+
+def remove_attrs(soup, whitelist=tuple()):
+    for tag in soup.findAll(True):
+        for attr in [attr for attr in tag.attrs if attr not in whitelist]:
+            del tag[attr]
+    return soup
 
 def get_posts(html_doc):
     soup = BeautifulSoup(html_doc, 'lxml')
@@ -37,8 +51,18 @@ def get_posts(html_doc):
     for x in tempuns:
         usernames.append(x.string.strip())
     tempps = soup.findAll('div', id=re.compile('^post_message_.*'))
-    for x in tempps:
-        posts.append(x.text.strip().replace("\t","").replace("\n\n\n","\n"))
+    for y in tempps:
+        quote = y.find('table')
+        if (quote is not None):
+            quote = remove_attrs(quote)
+            quote_text = '\t'+quote.text.strip().replace('\n','\n\t')+'\n\n'
+            y.div.decompose()
+            #posts.append('**********\n\n'+str(quote_text)+'\n\n**********\n\n'+y.text.strip())
+            posts.append(str(quote_text)+y.text.strip())
+        else:
+            posts.append (y.text.strip())
+    #for x in tempps:
+    #    posts.append(x.text.strip().replace("\t","").replace("\n\n\n","\n"))
     return (title, zip(usernames, posts))
 
 def print_posts(title, posts):
